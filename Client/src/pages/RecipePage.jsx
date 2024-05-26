c46559d78b196f2539a4d878a06038233c55ddf2
Popa Rares Andrei <R.A.Popa@student.tudelft.nl>
2024-05-27 00:46:48 +0200

    Pagina retete gata

--- Client/src/pages/RecipePage.jsx
+++ Client/src/pages/RecipePage.jsx
@@ -110,13 +110,57 @@ const RecipePage = () =>{
                             </div>
                         </div>
                     </div>
-
+                    <div className="ingredients">
+                        <div className="title">
+                            Ingrediente
+                        </div>
+                            <div className="list">
+                                <div className="ingredient">
+                                    <div className="ingredientText">
+                                        4 bucăți de steak de vită
+                                    </div>
+                                </div>
+                                <div className="ingredient">
+                                    <div className="ingredientText">
+                                        4 cartofi
+                                    </div>
+                                </div>
+                                <div className="ingredient">
+                                    <div className="ingredientText">
+                                        2 linguri de ulei de măsline
+                                    </div>
+                                </div>
+                                <div className="ingredient">
+                                    <div className="ingredientText">
+                                        1 linguriță de sare
+                                    </div>
+                                </div>
+                                <div className="ingredient">
+                                    <div className="ingredientText">
+                                        1 linguriță de piper
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     <div className="description">
                         <div className="title">
                             Descriere
                         </div>
+                        
                         <div className="text">
-                        Carnea de vită este apreciată pentru textura sa suculentă și gustul bogat, oferind o experiență culinară satisfăcătoare și plină de savoare. Steak-ul de vită poate fi preparat în diferite feluri, de la rare (sângeriu), medium-rare (puțin sângeriu în mijloc), medium (roz în mijloc) până la well-done (bine făcut), pentru a se potrivi preferințelor individuale ale fiecărui consumator. 
+                        Prima dată am întins bucata de aluat (eu am tăiat o fâșie de aproximativ 2.7 cm lățime dintr-o porție întreagă de foitaj cu unt) într-o fâșie de aproximativ 37 cm, lată de 10 cm și groasă de 2-3 mm. Am decupat cu ajutorul unei forme de stică 4 cercuri, le-am pus pe o bucată de foaie de copt, le-am înțepat de câteva ori cu o furculiță, le-am acoperit cu altă bucată de foaie de copt și le-am pus în frigider până am pregătit restul lucrurilor. Am uns pereții laterali ai formelor cu puțin unt și le-am lăsat deoparte.
+
+Pentru caramel, am pus cele 100 g zahăr în cratița pregătită și am pus-o pe foc mic până zahărul s-a topit și s-a transformat într-un lichid maroniu, vâscos și fin (am mișcat de cratița de câteva ori pentru ca zahărul să se topească uniform) – durează cam 10-15 minute acest proces.
+
+Cât s-a format caramelul, am curățat merele de coajă, le-am pus în accesoriul de tăiere felii de la robot și am tăiat merele fâșii.
+
+Am feliat mai întâi primul mar și, din foaia obținută, am făcut o rolă – practic, am rulat fâșia de măr până am reconstruit din ea mărul. Am făcut la fel cu al doilea măr.
+
+Am feliat și al treilea măr, dar pe ăsta nu l-am rulat individuală, ci l-am adăugat la relele obținute din primele 2 mere, jumi-juma la fiecară- – ca să obțin practic 2 role groase cât forma mea de copt. „Rola” trebuie să fie puțin mai subțire decât circumferința formelor de sticlă termorezinstentă folosite.
+
+Am tăiat fiecare „rolă” de măr obținută în două și le-am pus deoparte.
+
+
                         </div>
                     </div>
                 </div>
