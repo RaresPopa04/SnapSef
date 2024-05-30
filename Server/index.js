d481840aad3f18fa601a8d0954d5828f278f7969
Popa Rares Andrei <R.A.Popa@student.tudelft.nl>
2024-05-30 23:57:19 +0200

    User model + signup

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const profile = require("./routes/profile");
+const connectDb = require("./config/connectDb");
 
+connectDb();
 app.use("/profile",profile);
 
 
--- Server/routes/profile.js
+++ Server/routes/profile.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
 
-router.get("/signup",(req,res)=>{
-    res.send("Register");
+router.get("/signup",async (req,res)=>{
+
+    const user = req.body;
+
+    const newUser = new User({
+        name:user.name,
+        email:user.email,
+        password:user.password
+    })
+
+    newUser.save().then(()=>{
+        res.json("User saved");
+    }).catch((err)=>{
+        res.json(err);
+    })
+
 })
 
 module.exports = router;
