2f4f5e4b9071e10df48c57b0b33f0e9703707757
Popa Rares Andrei <R.A.Popa@student.tudelft.nl>
2024-05-31 23:00:53 +0200

    Signup route

--- Server/models/User.js
+++ Server/models/User.js
@@ -3,24 +3,28 @@ const bcrypt = require("bcryptjs");
 
 
 const userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        min:6,
-        max:255
-    },
-    email:{
-        type:String,
-        required:true,
-        min:6,
-        max:255
-    },
-    password:{
-        type:String,
-        required:true,
-        min:6,
-        max:1024
-    }
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+      firstName: {
+        type: String,
+        required: false,
+      },
+      lastName: {
+        type: String,
+        required: false,
+      }
 }
 )
 
--- Server/routes/profile.js
+++ Server/routes/profile.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 router.post("/signup",async (req,res)=>{
 
     const user = req.body;
 
     const newUser = new User({
-        name:user.name,
+        username:user.username,
         email:user.email,
-        password:user.password
+        password:user.password,
+        firstName:user.firstName,
+        lastName:user.lastName
     })
 
     newUser.save().then(()=>{
@@ -18,6 +21,24 @@ router.post("/signup",async (req,res)=>{
         res.json(err);
     })
 
+})
+
+router.post("/login",async (req,res)=>{
+    const userDetails = req.body;
+    const user = await User.findOne({email:userDetails.email})
+    if(!user){
+        res.json("User not found");
+    }
+    else{
+        const password =await bcrypt.compare(userDetails.password,user.password);
+        if(!password){
+            res.json("Password incorrect");
+        }else{
+            let userWithoutPassword = user.toObject()
+            delete userWithoutPassword.password;
+            res.json(userWithoutPassword);
+        }
+    }
 })
 
 module.exports = router;
