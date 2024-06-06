dc0d8b07fa06acb1b07f2ba578036af049699dca
Popa Rares Andrei <R.A.Popa@student.tudelft.nl>
2024-06-06 23:20:21 +0200

    Edit recipe

--- Server/routes/recipe.js
+++ Server/routes/recipe.js
@@ -44,9 +44,45 @@ router.post("/newRecipe", async (req,res)=>{
         
 
     }
+})
+router.put("/edit/:id", async (req,res)=>{
+    try {
+        const recipe = req.body;
+        const token = req.headers.authorization.split(" ")[1];
+
+        if(!token){
+            res.status(400).send("Nu ai acces");
+        }
+
+        const {username,cook} = jwt.verify(token,process.env.JWT_SECRET);
+        if(!cook) res.status(400).send("Nu esti bucatar");
+
+        const cookFound = await Cook.findOne({username:username});
+        if(!cookFound) res.status(400).send("Bucatarul nu mai exista");
+        const recipeId = req.params.id;
+        const newRecipe = await Recipe.findByIdAndUpdate({_id:recipeId,cook:cookFound._id},{
+            title: recipe.title,
+            ingredients: recipe.ingredients,
+            instructions: recipe.instructions,
+            time: recipe.time,
+            difficulty: recipe.difficulty,
+            servings: recipe.servings,
+            rating: recipe.rating,
+            calories: recipe.calories,
+            image: recipe.image,
+            fats: recipe.fats,
+            protein: recipe.protein,
+            carbs: recipe.carbs,
+
+        })
+
+        if(!newRecipe) res.status(400).send("Reteta nu exista");
+        res.status(200).send("Reteta actualizata cu succes");
+        
+    } catch (error) {
+        res.status(500).send("Eroare la server");
+    }
     
-
-
 })
 
 module.exports = router;
