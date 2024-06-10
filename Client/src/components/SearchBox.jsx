ceceb7d67fb31875dae77d5354f77d026052710f
Popa Rares Andrei <R.A.Popa@student.tudelft.nl>
2024-06-10 23:57:48 +0200

    Search box works

--- Client/src/components/SearchBox.jsx
+++ Client/src/components/SearchBox.jsx
@@ -1,9 +1,22 @@
 import { faSearch, faSliders } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 import "../styles/components/SearchBox.css";
+import { useNavigate } from "react-router-dom";
 
 const SearchBox = () => {
+
+    const navigate = useNavigate();
+    const [search,setSearch] = useState("");
+
+    const changeSearchText = (e)=>{
+        setSearch(e.target.value);
+    }
+
+    const handleSubmit = ()=>{
+        navigate(`/retete?titlu=${search}`);
+    }
+
     return(
         <div className="searchContainer">
                 <div className="searchBox">
@@ -11,9 +24,14 @@ const SearchBox = () => {
                         <FontAwesomeIcon className="icon" icon={faSearch} />
 
                     </div>
-                    <input type="text" placeholder="Caută rețete" />
+                    <input type="text" value = {search} onChange={changeSearchText} placeholder="Caută rețete" />
+
 
                 </div>
+                
+                <div className="submitBtn" onClick={handleSubmit}>
+                        Caută
+                    </div>
                 <div className="filter">
                     <FontAwesomeIcon icon={faSliders} />
                 </div>
